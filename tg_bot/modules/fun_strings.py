21d1b3d5d845c4a2670f34a10cbed8db8ac9cf65
lepatriebot <73965126+lepatriebot@users.noreply.github.com>
2020-11-13 04:56:48 +0530

    No need

--- tg_bot/modules/fun_strings.py
+++ tg_bot/modules/fun_strings.py
@@ -76,25 +76,6 @@ HIT = (
     "smacks",
     "bashes",
 )
-
-ABUSE_STRINGS = (
-    "Jaa na Bsdk, gaand mara jaake.",
-    "Tu paidaishi chutiya hai ki koi course kiya hai? ",
-    "I came to know that some shops were selling your ass for 69$",
-    "The world would have been so smooth if your dad had just pulled out",
-    "And the truth is, you're a fucking cunt",
-    "What else can you do other than spreading your legs?",
-    "Jaa na Gandu",
-    "Aand ka na Gaand ka, Gyaan jhaare pure Brahmand ka",
-    "Dhaat teri maa ki ch*t",
-    "Gaand se tatti nikalke jaadugar samajhta hai apne aap ko?",
-    "Bitches be trippin'",
-    "Please fuck off bitch, and get a life",
-    "Tu aise nhi maanega - Mat maan, maa chuda",
-    "Tujhse zada sundar teri jali hui gaand hai",
-    "Ye aapki Randikhana nhi hai, kripya yaha se dur rahe",
-    "I'm a good girl, I don't abuse. But you're a bitch.",
-)
     
 EYES = [
     ['⌐■', '■'],
